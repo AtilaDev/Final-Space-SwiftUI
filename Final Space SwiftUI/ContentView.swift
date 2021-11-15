ad8d0dc02f940edae6c92f9622610a28d163ca6c
Leandro <atiladevelop@gmail.com>
2021-11-14 21:41:31 -0300

    Initial Commit

---
+++ Final Space SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Final Space SwiftUI
+//
+//  Created by Leandro Favre on 14/11/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Final Space SwiftUI/Final_Space_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Final_Space_SwiftUIApp.swift
+//  Final Space SwiftUI
+//
+//  Created by Leandro Favre on 14/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct Final_Space_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
